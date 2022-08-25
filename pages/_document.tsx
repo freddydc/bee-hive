832cfda2aedff2da298e0930be50ee26693eb016
freddydc <freddydiaz.liv@gmail.com>
2022-08-25 10:52:15 -0500

    Add _document page

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+MyDocument.getInitialProps = async ctx => {
+  const initialProps = await Document.getInitialProps(ctx)
+  return initialProps
+}
+
+export default MyDocument
