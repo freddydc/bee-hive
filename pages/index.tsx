b72cb217be1a0300b2fa6ef738aa41c68f941dc8
freddydc <freddydiaz.liv@gmail.com>
2022-08-26 12:13:24 -0500

    List products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
 import styles from '@styles/Home.module.css'
+import { useEffect } from 'react'
 import { Layout } from '@components/Layout'
 
+const fetchProducts = () =>
+  fetch(process.env.NEXT_PUBLIC_API_URL ?? '/api/hello', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({
+      query: `{
+        productCollection(limit: 5) {
+          items {
+            name
+          }
+        }
+      }
+      `,
+    }),
+  })
+
 const Home = () => {
+  useEffect(() => {
+    fetchProducts()
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <Layout>
       <h1 className={styles.title}>Hello!</h1>
